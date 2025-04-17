9e1303c6a3e9b847bf481905e924901c81bb4ded
yuuki08noah <yuuki08noah@gmail.com>
2025-04-18 00:27:00 +0900

    Fix :: JPA 관계 설정

--- src/main/java/windeath44/server/memorial/domain/domain/MemorialUpdateHistory.java
+++ src/main/java/windeath44/server/memorial/domain/domain/MemorialUpdateHistory.java
@@ -1,14 +1,25 @@
 package windeath44.server.memorial.domain.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class MemorialUpdateHistory {
-  @Id @GeneratedValue(GenerationType.IDENTITY)
-  private Long memorialUpdateHistoryId;
-
-
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long memorial_update_history_id;
+  private Long user_id;
+  @Setter
+  @Getter
+  @ManyToOne
+  @JoinColumn(name = "memorial_id")
+  private Memorial memorial;
+  @Setter
+  @Getter
+  @ManyToOne
+  @JoinColumn(name = "memorial_commit_id")
+  private MemorialCommit memorial_commit;
+  private LocalDateTime updated_at = LocalDateTime.now();
 }
