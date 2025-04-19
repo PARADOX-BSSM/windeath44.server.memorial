7e3c6afef1bcc90c549cbf0781f5c52f1ddfe392
yuuki08noah <yuuki08noah@gmail.com>
2025-04-19 19:28:58 +0900

    feat :: MemorialUpdateHistoryMapper #5

---
+++ src/main/java/windeath44/server/memorial/domain/service/mapper/MemorialUpdateHistoryMapper.java
@@ -0,0 +1,18 @@
+package windeath44.server.memorial.domain.service.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import windeath44.server.memorial.domain.domain.Memorial;
+import windeath44.server.memorial.domain.domain.MemorialCommit;
+import windeath44.server.memorial.domain.domain.MemorialUpdateHistory;
+import windeath44.server.memorial.domain.presentation.dto.request.MemorialUpdateHistoryRequestDto;
+
+@Mapper(componentModel = "spring")
+public interface MemorialUpdateHistoryMapper {
+  MemorialUpdateHistoryMapper INSTANCE = Mappers.getMapper(MemorialUpdateHistoryMapper.class);
+
+  @Mapping(source = "memorialUpdateHistoryRequestDto.user_id", target = "user_id")
+  MemorialUpdateHistory toMemorialUpdateHistory(MemorialUpdateHistoryRequestDto memorialUpdateHistoryRequestDto, MemorialCommit memorial_commit);
+}
