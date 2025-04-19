3b6e41dc214faf5a06584b5bdba5901227a63562
yuuki08noah <yuuki08noah@gmail.com>
2025-04-19 20:20:33 +0900

    feat :: 마지막 커밋된(APPROVE 상태인) 커밋 반환 메서드 #5

--- src/main/java/windeath44/server/memorial/domain/domain/repository/MemorialCommitRepository.java
+++ src/main/java/windeath44/server/memorial/domain/domain/repository/MemorialCommitRepository.java
@@ -2,8 +2,11 @@ package windeath44.server.memorial.domain.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import windeath44.server.memorial.domain.domain.Memorial;
 import windeath44.server.memorial.domain.domain.MemorialCommit;
+import windeath44.server.memorial.domain.domain.MemorialCommitState;
 
 @Repository
 public interface MemorialCommitRepository extends JpaRepository<MemorialCommit, Long> {
+  MemorialCommit findMemorialCommitByMemorialAndState(Memorial memorial, MemorialCommitState state);
 }
