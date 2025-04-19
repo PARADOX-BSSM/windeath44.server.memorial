66c9c802942220f2e9cd607b743fa471d7dfdaf9
yuuki08noah <yuuki08noah@gmail.com>
2025-04-19 18:03:59 +0900

    fix :: MapStruct 수정

--- src/main/java/windeath44/server/memorial/domain/service/mapper/MemorialCommitMapper.java
+++ src/main/java/windeath44/server/memorial/domain/service/mapper/MemorialCommitMapper.java
@@ -1,13 +1,19 @@
 package windeath44.server.memorial.domain.service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import windeath44.server.memorial.domain.domain.Memorial;
 import windeath44.server.memorial.domain.domain.MemorialCommit;
 import windeath44.server.memorial.domain.presentation.dto.request.MemorialCommitRequestDto;
+import windeath44.server.memorial.domain.presentation.dto.response.MemorialResponseDto;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface MemorialCommitMapper {
   MemorialCommitMapper INSTANCE = Mappers.getMapper(MemorialCommitMapper.class);
 
-  MemorialCommit toMemorialCommit(MemorialCommitRequestDto requestDto);
+//  @Mapping(source = "memorialCommitRequestDto.user_id", target = "user_id")
+//  @Mapping(source = "memorialCommitRequestDto.content", target = "content")
+//  @Mapping(source = "memorial", target = )
+  MemorialCommit toMemorialCommit(MemorialCommitRequestDto memorialCommitRequestDto, Memorial memorial);
 }
