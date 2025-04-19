a894039cdf8deebc0a11b5ae304e5d1dedd484f7
yuuki08noah <yuuki08noah@gmail.com>
2025-04-19 19:29:36 +0900

    feat :: /merge 추가 #5

--- src/main/java/windeath44/server/memorial/domain/presentation/MemorialCreateController.java
+++ src/main/java/windeath44/server/memorial/domain/presentation/MemorialCreateController.java
@@ -2,12 +2,10 @@ package windeath44.server.memorial.domain.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import windeath44.server.memorial.domain.presentation.dto.global.ResponseDto;
 import windeath44.server.memorial.domain.presentation.dto.request.MemorialCommitRequestDto;
+import windeath44.server.memorial.domain.presentation.dto.request.MemorialUpdateHistoryRequestDto;
 import windeath44.server.memorial.domain.service.MemorialCommitPostService;
 
 @RestController
@@ -20,5 +18,11 @@ public class MemorialCreateController {
   public ResponseEntity<ResponseDto> commit(@RequestBody MemorialCommitRequestDto dto) {
     memorialCommitPostService.createMemorialCommit(dto);
     return ResponseEntity.ok(new ResponseDto(201, "Memorial Commit is successfully created.", null));
+  }
+
+  @PatchMapping("/merge")
+  public ResponseEntity<ResponseDto> merge(@RequestBody MemorialUpdateHistoryRequestDto dto) {
+    memorialCommitPostService.mergeMemorialCommit(dto);
+    return ResponseEntity.ok(new ResponseDto(201, "Memorial Commit is successfully merged.", null));
   }
 }
