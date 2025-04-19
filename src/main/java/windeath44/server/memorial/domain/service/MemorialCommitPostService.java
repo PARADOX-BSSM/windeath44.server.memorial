b4ee899a751b7258d4aaa28983e8a41e35b2b472
yuuki08noah <yuuki08noah@gmail.com>
2025-04-19 18:04:27 +0900

    fix :: Memorial JPA 관계는 일단 null로 해둠

--- src/main/java/windeath44/server/memorial/domain/service/MemorialCommitPostService.java
+++ src/main/java/windeath44/server/memorial/domain/service/MemorialCommitPostService.java
@@ -2,7 +2,9 @@ package windeath44.server.memorial.domain.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import windeath44.server.memorial.domain.domain.Memorial;
 import windeath44.server.memorial.domain.domain.repository.MemorialCommitRepository;
+import windeath44.server.memorial.domain.domain.repository.MemorialRepository;
 import windeath44.server.memorial.domain.presentation.dto.request.MemorialCommitRequestDto;
 import windeath44.server.memorial.domain.service.mapper.MemorialCommitMapper;
 
@@ -11,8 +13,10 @@ import windeath44.server.memorial.domain.service.mapper.MemorialCommitMapper;
 public class MemorialCommitPostService {
   private final MemorialCommitRepository memorialCommitRepository;
   private final MemorialCommitMapper memorialCommitMapper;
+//  private final MemorialRepository memorialRepository;
 
   public void createMemorialCommit(MemorialCommitRequestDto memorialCommitRequestDto) {
-    memorialCommitRepository.save(memorialCommitMapper.toMemorialCommit(memorialCommitRequestDto));
+//    Memorial memorial = memorialRepository.findMemorial(memorialCommitRequestDto.memorial_id());
+    memorialCommitRepository.save(memorialCommitMapper.toMemorialCommit(memorialCommitRequestDto, null));
   }
 }
