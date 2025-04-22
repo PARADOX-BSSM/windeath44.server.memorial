8d9ee271373182d4f6b0cc1e3b0808417e2ee71f
yuuki08noah <yuuki08noah@gmail.com>
2025-04-22 17:56:59 +0900

    feat :: ExceptionHandler #5

---
+++ src/main/java/windeath44/server/memorial/domain/presentation/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package windeath44.server.memorial.domain.presentation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import windeath44.server.memorial.domain.exception.MemorialCommitNotFoundException;
+import windeath44.server.memorial.domain.exception.MemorialNotFoundException;
+import windeath44.server.memorial.domain.presentation.dto.global.ErrorResponseDto;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(MemorialNotFoundException.class)
+  protected ResponseEntity<ErrorResponseDto> handleMemorialNotFoundException(MemorialNotFoundException e) {
+    log.error(e.getMessage(), e);
+    final ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage());
+    return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(MemorialCommitNotFoundException.class)
+  protected ResponseEntity<ErrorResponseDto> handleMemorialCommitNotFoundException(MemorialCommitNotFoundException e) {
+    log.error(e.getMessage(), e);
+    final ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage());
+    return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+  }
+}
