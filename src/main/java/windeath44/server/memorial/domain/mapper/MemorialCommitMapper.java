fdf6da898d14b8bd1959c3a34ec4592ab294f054
yuuki08noah <yuuki08noah@gmail.com>
2025-04-24 17:21:36 +0900

    feat :: toMemorialCommitResponseDto #5

--- src/main/java/windeath44/server/memorial/domain/mapper/MemorialCommitMapper.java
+++ src/main/java/windeath44/server/memorial/domain/mapper/MemorialCommitMapper.java
@@ -5,10 +5,12 @@ import org.mapstruct.factory.Mappers;
 import windeath44.server.memorial.domain.entity.Memorial;
 import windeath44.server.memorial.domain.entity.MemorialCommit;
 import windeath44.server.memorial.domain.presentation.dto.request.MemorialCommitRequestDto;
+import windeath44.server.memorial.domain.presentation.dto.response.MemorialCommitResponseDto;
 
 @Mapper(componentModel = "spring")
 public interface MemorialCommitMapper {
   MemorialCommitMapper INSTANCE = Mappers.getMapper(MemorialCommitMapper.class);
 
   MemorialCommit toMemorialCommit(MemorialCommitRequestDto memorialCommitRequestDto, Memorial memorial);
+  MemorialCommitResponseDto toMemorialCommitResponseDto(MemorialCommit memorialCommit);
 }
