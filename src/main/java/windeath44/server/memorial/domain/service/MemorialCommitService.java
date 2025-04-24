3c04f9b467487959202341043bf3abc212e40a34
yuuki08noah <yuuki08noah@gmail.com>
2025-04-24 17:21:20 +0900

    feat :: MemorialCommit 생성 시 반환 #5

--- src/main/java/windeath44/server/memorial/domain/service/MemorialCommitService.java
+++ src/main/java/windeath44/server/memorial/domain/service/MemorialCommitService.java
@@ -4,22 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import windeath44.server.memorial.domain.entity.Memorial;
 import windeath44.server.memorial.domain.entity.MemorialCommit;
-import windeath44.server.memorial.domain.entity.MemorialPullRequestState;
-import windeath44.server.memorial.domain.entity.MemorialPullRequest;
 import windeath44.server.memorial.domain.entity.repository.MemorialCommitRepository;
 import windeath44.server.memorial.domain.entity.repository.MemorialRepository;
-import windeath44.server.memorial.domain.entity.repository.MemorialPullRequestRepository;
 import windeath44.server.memorial.domain.exception.*;
 import windeath44.server.memorial.domain.presentation.dto.request.MemorialCommitRequestDto;
-import windeath44.server.memorial.domain.presentation.dto.request.MemorialMergeRequestDto;
-import windeath44.server.memorial.domain.presentation.dto.request.MemorialPullRequestDto;
 import windeath44.server.memorial.domain.mapper.MemorialCommitMapper;
-import windeath44.server.memorial.domain.presentation.dto.response.CompareContentsResponseDto;
-import windeath44.server.memorial.domain.presentation.dto.response.MemorialMergeableResponseDto;
-import windeath44.server.memorial.global.diff_match_patch;
-import windeath44.server.memorial.global.diff_match_patch.Diff;
-
-import java.util.LinkedList;
+import windeath44.server.memorial.domain.presentation.dto.response.MemorialCommitResponseDto;
 
 @Service
 @RequiredArgsConstructor
@@ -28,9 +18,10 @@ public class MemorialCommitService {
   private final MemorialCommitMapper memorialCommitMapper;
   private final MemorialRepository memorialRepository;
 
-  public void createMemorialCommit(MemorialCommitRequestDto memorialCommitRequestDto) {
+  public MemorialCommitResponseDto createMemorialCommit(MemorialCommitRequestDto memorialCommitRequestDto) {
     Memorial memorial = memorialRepository.findById(memorialCommitRequestDto.memorialId())
             .orElseThrow(MemorialNotFoundException::new);
-    memorialCommitRepository.save(memorialCommitMapper.toMemorialCommit(memorialCommitRequestDto, memorial));
+    MemorialCommit memorialCommit = memorialCommitRepository.save(memorialCommitMapper.toMemorialCommit(memorialCommitRequestDto, memorial));
+    return memorialCommitMapper.toMemorialCommitResponseDto(memorialCommit);
   }
 }
