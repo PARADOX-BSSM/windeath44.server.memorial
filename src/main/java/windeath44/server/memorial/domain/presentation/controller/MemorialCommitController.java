b26c61eb18d8a538a5b3c8204eed930fc305f85c
yuuki08noah <yuuki08noah@gmail.com>
2025-04-24 17:22:04 +0900

    chore :: 불필요한 import 제거 #5

--- src/main/java/windeath44/server/memorial/domain/presentation/controller/MemorialCommitController.java
+++ src/main/java/windeath44/server/memorial/domain/presentation/controller/MemorialCommitController.java
@@ -5,20 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import windeath44.server.memorial.domain.presentation.dto.ResponseDto;
 import windeath44.server.memorial.domain.presentation.dto.request.MemorialCommitRequestDto;
-import windeath44.server.memorial.domain.presentation.dto.request.MemorialMergeRequestDto;
-import windeath44.server.memorial.domain.presentation.dto.request.MemorialPullRequestDto;
-import windeath44.server.memorial.domain.presentation.dto.response.MemorialMergeableResponseDto;
 import windeath44.server.memorial.domain.service.MemorialCommitService;
-import windeath44.server.memorial.domain.service.MemorialMergeService;
-import windeath44.server.memorial.domain.service.MemorialPullRequestService;
 
 @RestController
 @RequestMapping("/memorials")
 @RequiredArgsConstructor
 public class MemorialCommitController {
   private final MemorialCommitService memorialCommitService;
-  private final MemorialPullRequestService memorialPullRequestService;
-  private final MemorialMergeService memorialMergeService;
 
   @PostMapping("/commit")
   public ResponseEntity<ResponseDto> commit(@RequestBody MemorialCommitRequestDto dto) {
