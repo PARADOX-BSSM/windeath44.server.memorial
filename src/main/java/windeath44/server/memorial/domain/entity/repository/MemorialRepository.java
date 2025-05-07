e5c35942b8b419cec67aad940d720f9bed7eb60d
yuuki08noah <yuuki08noah@gmail.com>
2025-05-07 19:07:38 +0900

    chore :: 필요없는거 삭제 #3

--- src/main/java/windeath44/server/memorial/domain/entity/repository/MemorialRepository.java
+++ src/main/java/windeath44/server/memorial/domain/entity/repository/MemorialRepository.java
@@ -1,18 +1,10 @@
 package windeath44.server.memorial.domain.entity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import windeath44.server.memorial.domain.entity.Memorial;
 
-import java.util.List;
-
 @Repository
-public interface MemorialRepository extends JpaRepository<Memorial, Long> {
-  @Query(value = "select * from " +
-          "memorial join memorial_update_history history on memorial.memorial_id = history.memorial_id join " +
-          "memorial_commit commit on history.memorial_commit_id = commit.memorial_commit_id ", nativeQuery = true)
-  List<Object[]> findMemorials();
-
+public interface MemorialRepository extends JpaRepository<Memorial, Long>, MemorialRepositoryCustom {
 
 }
