7f8355929b707f91376d173428ce7076bc209edf
yuuki08noah <yuuki08noah@gmail.com>
2025-06-03 20:42:02 +0900

    fix :: [BACKEND-21] 오류 수정

--- src/main/java/windeath44/server/memorial/domain/controller/MemorialBowController.java
+++ src/main/java/windeath44/server/memorial/domain/controller/MemorialBowController.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import windeath44.server.memorial.domain.dto.ResponseDto;
 import windeath44.server.memorial.domain.dto.request.MemorialBowRequestDto;
+import windeath44.server.memorial.domain.service.MemorialBowService;
 
 @RestController
 @RequestMapping("/memorials")
 @RequiredArgsConstructor
 public class MemorialBowController {
+  private final MemorialBowService memorialBowService;
+
   @PostMapping("/bow")
   public ResponseEntity<ResponseDto> bow(@RequestBody MemorialBowRequestDto memorialBowRequestDto) {
-
-    return ResponseEntity.ok(new ResponseDto("UserId " + memorialBowRequestDto.userId() + " Successfully bowed to " + memorialBowRequestDto.memorialId(), false));
+    memorialBowService.bow(memorialBowRequestDto);
+    return ResponseEntity.ok(new ResponseDto("User id: " + memorialBowRequestDto.userId() + " successfully bowed to memorial id:" + memorialBowRequestDto.memorialId(), null));
   }
 }
