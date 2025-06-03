acca44a4af58cd423ceaf93a321af623d4769fd8
yuuki08noah <yuuki08noah@gmail.com>
2025-06-03 21:46:45 +0900

    feat :: [BACKEND-21] 익셉션 추가

--- src/main/java/windeath44/server/memorial/domain/service/MemorialBowService.java
+++ src/main/java/windeath44/server/memorial/domain/service/MemorialBowService.java
@@ -3,17 +3,25 @@ package windeath44.server.memorial.domain.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import windeath44.server.memorial.domain.dto.request.MemorialBowRequestDto;
+import windeath44.server.memorial.domain.exception.MemorialNotFoundException;
+import windeath44.server.memorial.domain.model.Memorial;
 import windeath44.server.memorial.domain.model.MemorialBow;
 import windeath44.server.memorial.domain.repository.MemorialBowRepository;
+import windeath44.server.memorial.domain.repository.MemorialRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MemorialBowService {
   private final MemorialBowRepository memorialBowRepository;
+  private final MemorialRepository memorialRepository;
 
   public void bow(MemorialBowRequestDto memorialBowRequestDto) {
     String userId = memorialBowRequestDto.userId();
     Long memorialId = memorialBowRequestDto.memorialId();
+    Memorial memorial = memorialRepository.findById(memorialId).orElse(null);
+    if (memorial == null) {
+      throw new MemorialNotFoundException();
+    }
     MemorialBow memorialBow = memorialBowRepository.findMemorialBowByUserIdAndMemorialId(userId, memorialId);
     if(memorialBow == null) {
       MemorialBow newMemorialBow = new MemorialBow(
