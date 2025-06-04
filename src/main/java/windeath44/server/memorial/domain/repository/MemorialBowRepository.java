07eb07a4453558d8abf0fb34d168a0d5e2e5e7b5
yuuki08noah <yuuki08noah@gmail.com>
2025-06-04 17:06:58 +0900

    feat :: [BACKEND-23] JPQL로 집계

--- src/main/java/windeath44/server/memorial/domain/repository/MemorialBowRepository.java
+++ src/main/java/windeath44/server/memorial/domain/repository/MemorialBowRepository.java
@@ -1,10 +1,16 @@
 package windeath44.server.memorial.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import windeath44.server.memorial.domain.model.MemorialBow;
+
+import java.util.List;
 
 @Repository
 public interface MemorialBowRepository extends JpaRepository<MemorialBow, Long> {
   MemorialBow findMemorialBowByUserIdAndMemorialId(String userId, Long memorialId);
+  @Query("select sum(m.bowCount) from MemorialBow m where m.memorialId = :memorialId")
+  Long sumBowCount(@Param("memorialId") Long memorialId);
 }
