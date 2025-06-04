3d5c5dc557f451a6293c2a455cb7781a873d7f4a
yuuki08noah <yuuki08noah@gmail.com>
2025-06-04 17:18:37 +0900

    feat :: [BACKEND-23] /memorials/{userId}/{memorialId}

--- src/main/java/windeath44/server/memorial/domain/controller/MemorialBowController.java
+++ src/main/java/windeath44/server/memorial/domain/controller/MemorialBowController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import windeath44.server.memorial.domain.dto.ResponseDto;
 import windeath44.server.memorial.domain.dto.request.MemorialBowRequestDto;
+import windeath44.server.memorial.domain.dto.response.MemorialBowResponseDto;
 import windeath44.server.memorial.domain.service.MemorialBowService;
 
 @RestController
@@ -16,12 +17,18 @@ public class MemorialBowController {
   @PostMapping("/bow")
   public ResponseEntity<ResponseDto> bow(@RequestBody MemorialBowRequestDto memorialBowRequestDto) {
     memorialBowService.bow(memorialBowRequestDto);
-    return ResponseEntity.ok(new ResponseDto("User id: " + memorialBowRequestDto.userId() + " successfully bowed to memorial id:" + memorialBowRequestDto.memorialId(), null));
+    return ResponseEntity.status(201).body(new ResponseDto("User id: " + memorialBowRequestDto.userId() + " successfully bowed to memorial id:" + memorialBowRequestDto.memorialId(), null));
   }
 
   @GetMapping("/bow/{memorialId}")
-  public ResponseEntity<ResponseDto> getBow(@PathVariable Long memorialId) {
+  public ResponseEntity<ResponseDto> getBowByUserId(@PathVariable Long memorialId) {
     Long bowCount = memorialBowService.BowCountByMemorialId(memorialId);
     return ResponseEntity.ok(new ResponseDto("Memorial id: " + memorialId + " bow count: " + bowCount, bowCount));
+  }
+
+  @GetMapping("/bow/{userId}/{memorialId}")
+  public ResponseEntity<ResponseDto> getBowByUserIdAndMemorialId(@PathVariable String userId, @PathVariable Long memorialId) {
+    MemorialBowResponseDto memorialBowResponseDto = memorialBowService.findMemorialBowByUserIdAndMemorialId(userId, memorialId);
+    return ResponseEntity.ok(new ResponseDto("Memorial id: " + memorialId + " and user id: " + userId, memorialBowResponseDto));
   }
 }
