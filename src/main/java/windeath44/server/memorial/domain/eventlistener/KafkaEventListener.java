6b83daac229789651f2615d9b3c82bd367f16bc3
yuuki08noah <yuuki08noah@gmail.com>
2025-06-10 08:32:16 +0900

    feat :: [BACKEND-31] memorial-deletion-request

--- src/main/java/windeath44/server/memorial/domain/eventlistener/KafkaEventListener.java
+++ src/main/java/windeath44/server/memorial/domain/eventlistener/KafkaEventListener.java
@@ -1,20 +1,34 @@
 package windeath44.server.memorial.domain.eventlistener;
 
-import com.example.avro.MemorialAvroSchema;
+import com.example.avro.EventResultAvroSchema;
+import com.example.avro.MemorialCreationAvroSchema;
+import com.example.avro.MemorialDeletionAvroSchema;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import windeath44.server.memorial.domain.service.MemorialCreateService;
+import windeath44.server.memorial.domain.service.MemorialDeleteService;
 
 @Component
 @RequiredArgsConstructor
 public class KafkaEventListener {
   private final MemorialCreateService memorialCreateService;
+  private final MemorialDeleteService memorialDeleteService;
+  private final KafkaTemplate<String, Object> kafkaTemplate;
 
-  @KafkaListener(topics = "memorial-creation", groupId = "memorial")
+  @KafkaListener(topics = "memorial-creation-request", groupId = "memorial")
   @Transactional
-  public void listen(MemorialAvroSchema memorialAvroSchema) {
-    memorialCreateService.createMemorial(memorialAvroSchema);
+  public void memorialCreation(MemorialCreationAvroSchema memorialCreationAvroSchema) {
+    memorialCreateService.createMemorial(memorialCreationAvroSchema);
+    kafkaTemplate.send("memorial-creation-response", new EventResultAvroSchema(true));
+  }
+
+  @KafkaListener(topics = "memorial-deletion-request", groupId = "memorial")
+  @Transactional
+  public void memorialDeletion(MemorialDeletionAvroSchema memorialDeletionAvroSchema) {
+    memorialDeleteService.deleteMemorial(memorialDeletionAvroSchema);
+    kafkaTemplate.send("memorial-deletion-response", new EventResultAvroSchema(true));
   }
 }
