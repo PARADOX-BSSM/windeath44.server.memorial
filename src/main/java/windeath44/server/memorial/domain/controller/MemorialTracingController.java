b7a0f220eb01684cb1e1fee48449dd50a588331f
pdh0128 <pdh0128a@gmail.com>
2025-06-16 23:35:09 +0900

    feat :: user가 본 memorial 최근 조회

---
+++ src/main/java/windeath44/server/memorial/domain/controller/MemorialTracingController.java
@@ -0,0 +1,26 @@
+package windeath44.server.memorial.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import windeath44.server.memorial.domain.dto.ResponseDto;
+import windeath44.server.memorial.domain.dto.response.MemorialTracingResponse;
+import windeath44.server.memorial.domain.model.MemorialTracing;
+import windeath44.server.memorial.domain.service.MemorialTraceService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/memorial-tracing/{user-id}")
+public class MemorialTracingController {
+  private final MemorialTraceService memorialTraceService;
+
+  @GetMapping
+  public ResponseEntity<ResponseDto> getMemorialTracing(@PathVariable("user-id") String userId, @RequestParam("size") Integer size) {
+    List<MemorialTracingResponse> memorialTracingList = memorialTraceService.findRecentByUserId(userId, size);
+    ResponseDto responseDto = new ResponseDto("Memorial Tracing is Successfully Found", memorialTracingList);
+    return ResponseEntity.ok(responseDto);
+  }
+}
--- src/main/java/windeath44/server/memorial/domain/model/MemorialTracing.java
+++ src/main/java/windeath44/server/memorial/domain/model/MemorialTracing.java
@@ -1,16 +1,22 @@
 package windeath44.server.memorial.domain.model;
 
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Document(collection = "memorial_tracing")
 @Builder
+@Getter
 public class MemorialTracing {
   @Id
   private String memorialTracingId;
   private String userId;
   private Long memorialId;
+  @Builder.Default
+  private LocalDateTime viewed = LocalDateTime.now();
 
   public static MemorialTracing of(Long memorialId, String userId) {
     return windeath44.server.memorial.domain.model.MemorialTracing.builder()
