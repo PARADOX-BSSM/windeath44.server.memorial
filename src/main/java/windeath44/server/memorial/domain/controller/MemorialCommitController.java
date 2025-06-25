6e20b1cfec57b25396635a642a35ca632f0fa677
yuuki08noah <yuuki08noah@gmail.com>
2025-06-25 13:44:50 +0900

    fix :: [BACKEND-53] commit 단일 조회, 메모리얼별 조회

--- src/main/java/windeath44/server/memorial/domain/controller/MemorialCommitController.java
+++ src/main/java/windeath44/server/memorial/domain/controller/MemorialCommitController.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import windeath44.server.memorial.domain.dto.ResponseDto;
 import windeath44.server.memorial.domain.dto.request.MemorialCommitRequestDto;
+import windeath44.server.memorial.domain.dto.response.MemorialCommitResponseDto;
 import windeath44.server.memorial.domain.service.MemorialCommitService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/memorials")
@@ -17,5 +20,17 @@ public class MemorialCommitController {
   public ResponseEntity<ResponseDto> commit(@RequestBody MemorialCommitRequestDto dto) {
     memorialCommitService.createMemorialCommit(dto);
     return ResponseEntity.status(201).body(new ResponseDto("Memorial Commit is successfully created.", null));
+  }
+
+  @GetMapping("/commits/{memorialId}")
+  public ResponseEntity<ResponseDto> getCommits(@PathVariable Long memorialId) {
+    List<MemorialCommitResponseDto> memorialCommitResponseDtos = memorialCommitService.findMemorialCommitsByMemorialId(memorialId);
+    return ResponseEntity.status(201).body(new ResponseDto("Memorial Commits are successfully found.", memorialCommitResponseDtos));
+  }
+
+  @GetMapping("/commit/{commitId}")
+  public ResponseEntity<ResponseDto> getCommit(@PathVariable Long commitId) {
+    MemorialCommitResponseDto memorialCommitResponseDto = memorialCommitService.findMemorialCommitById(commitId);
+    return ResponseEntity.status(201).body(new ResponseDto("Memorial Commit is successfully found.", memorialCommitResponseDto));
   }
 }
