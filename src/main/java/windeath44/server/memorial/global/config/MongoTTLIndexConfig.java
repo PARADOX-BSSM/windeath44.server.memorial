836244ab80eb3552d6fc89a8fa82f3c87392335c
yuuki08noah <yuuki08noah@gmail.com>
2025-07-15 09:50:57 +0900

    fix :: [BACKEND-66] Tracing 임시 삭제

--- src/main/java/windeath44/server/memorial/global/config/MongoTTLIndexConfig.java
+++ src/main/java/windeath44/server/memorial/global/config/MongoTTLIndexConfig.java
@@ -1,22 +1,22 @@
-package windeath44.server.memorial.global.config;
-
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.Index;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-
-@Configuration
-public class MongoTTLIndexConfig {
-
-  @Bean
-  public ApplicationRunner ttlIndexCreator(MongoTemplate mongoTemplate) {
-    return args -> {
-      IndexOperations indexOps = mongoTemplate.indexOps("user_memorial_tracing");
-      indexOps.ensureIndex(new Index()
-              .on("viewed", org.springframework.data.domain.Sort.Direction.ASC)
-              .expire(2592000)); // 30일
-    };
-  }
-}
+//package windeath44.server.memorial.global.config;
+//
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.data.mongodb.core.index.Index;
+//import org.springframework.data.mongodb.core.index.IndexOperations;
+//
+//@Configuration
+//public class MongoTTLIndexConfig {
+//
+//  @Bean
+//  public ApplicationRunner ttlIndexCreator(MongoTemplate mongoTemplate) {
+//    return args -> {
+//      IndexOperations indexOps = mongoTemplate.indexOps("user_memorial_tracing");
+//      indexOps.ensureIndex(new Index()
+//              .on("viewed", org.springframework.data.domain.Sort.Direction.ASC)
+//              .expire(2592000)); // 30일
+//    };
+//  }
+//}
