93c5b7a181118b43c04075488f6ee6ad43b8fdf3
pdh0128 <pdh0128a@gmail.com>
2025-10-21 21:31:19 +0900

    fix :: memorial tracing path

--- src/main/java/windeath44/server/memorial/domain/memorial/controller/MemorialTracingController.java
+++ src/main/java/windeath44/server/memorial/domain/memorial/controller/MemorialTracingController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/memorial-tracing")
+@RequestMapping("/memorials/memorial-tracing")
 public class MemorialTracingController {
   private final MemorialTraceService memorialTraceService;
 
