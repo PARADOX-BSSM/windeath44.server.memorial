9938c3c4edf5ec9d3c45ff3769168641b7fd268c
yuuki08noah <yuuki08noah@gmail.com>
2025-10-27 22:10:34 +0900

    feat :: Authorization 추가 [PW-209]

--- src/main/java/windeath44/server/memorial/domain/memorial/service/MemorialResolveService.java
+++ src/main/java/windeath44/server/memorial/domain/memorial/service/MemorialResolveService.java
@@ -8,6 +8,7 @@ import windeath44.server.memorial.domain.memorial.model.MemorialPullRequestState
 import windeath44.server.memorial.domain.memorial.repository.MemorialCommitRepository;
 import windeath44.server.memorial.domain.memorial.repository.MemorialPullRequestRepository;
 import windeath44.server.memorial.domain.memorial.exception.MemorialPullRequestNotFoundException;
+import windeath44.server.memorial.domain.memorial.exception.MemorialMergePermissionDeniedException;
 import windeath44.server.memorial.domain.memorial.dto.request.MemorialCommitRequestDto;
 import windeath44.server.memorial.domain.memorial.dto.request.MemorialMergeRequestDto;
 import windeath44.server.memorial.domain.memorial.dto.request.MemorialPullRequestRequestDto;
@@ -23,12 +24,19 @@ public class MemorialResolveService {
   private final MemorialCommitService memorialCommitService;
   private final MemorialPullRequestService memorialPullRequestService;
   private final MemorialMergeService memorialMergeService;
+  private final MemorialChiefService memorialChiefService;
 
   public void resolve(String userId, MemorialResolveRequestDto memorialResolveRequestDto) {
     MemorialPullRequest memorialPullRequest = memorialPullRequestRepository.findById(memorialResolveRequestDto.memorialPullRequestId())
             .orElseThrow(MemorialPullRequestNotFoundException::new);
-    memorialPullRequest.setState(MemorialPullRequestState.RESOLVED);
     Memorial memorial = memorialPullRequest.getMemorial();
+
+    // Check if user is a chief of this memorial
+    if (!memorialChiefService.getChiefs(memorial.getMemorialId()).contains(userId)) {
+      throw new MemorialMergePermissionDeniedException();
+    }
+
+    memorialPullRequest.setState(MemorialPullRequestState.RESOLVED);
     Long memorialId = memorial.getMemorialId();
     String resolved = memorialResolveRequestDto.resolved();
     MemorialCommitRequestDto memorialCommitRequestDto = new MemorialCommitRequestDto(memorialId, resolved);
